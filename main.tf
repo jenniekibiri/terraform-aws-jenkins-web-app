02e404da62b271ca8b8f6b91b49384a2737ac21e
jenny <josphatje@gmail.com>
2022-06-16 20:14:56 +0300

    replace EOF with script file

--- main.tf
+++ main.tf
@@ -124,13 +124,7 @@ resource "aws_instance" "myapp-server" {
 
   associate_public_ip_address = true
   key_name                    = aws_key_pair.ssh-key.key_name
-  user_data                   = <<EOF
-#!/bin/bash
-sudo yum update -y && sudo yum install -y docker
-sudo systemctl start docker 
-sudo usermod -aG docker ec2-user
-docker run -p 8080:80 nginx
-EOF
+  user_data                   = file("entry-script.sh")
 
   tags = {
     Name : "${var.env_prefix}-server"
