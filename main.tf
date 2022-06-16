23194ff25875c71126568313483d5ba6039a379c
jenny <josphatje@gmail.com>
2022-06-16 10:14:07 +0300

    create-custom-route-table and subnet association

--- main.tf
+++ main.tf
@@ -5,7 +5,10 @@ provider "aws" {
 variable vpc_cidr_block {}
 variable subnet_cidr_block {}
 variable avail_zone {}
-variable "env_prefix" {}
+variable env_prefix {}
+variable my_ip{}
+  
+
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -22,4 +25,65 @@ resource "aws_subnet"  "myapp-subnet-1" {
     Name : "${var.env_prefix}-subnet-1"
   }
 
+}
+resource "aws_internet_gateway" "myapp-igw" {
+ vpc_id = aws_vpc.myapp-vpc.id
+  tags={
+    Name:"${var.env_prefix}-igw"
+  }
+  
+}
+
+resource "aws_route_table" "myapp-route-table" {
+  #which VPC this route table belongs to
+  vpc_id=aws_vpc.myapp-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+
+  }
+  tags = {
+    Name : "${var.env_prefix}-rtb"
+  }
+  
+}
+
+resource "aws_route_table_association" "a-rtb-subnet" {
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id
+  
+}
+
+
+
+
+resource "aws_default_route_table" "main_rtb" {
+   default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+   tags={
+    Name:"${var.env_prefix}-main-rtb"
+   }
+
+}
+
+
+resource "aws_default_security_group" "default-sg" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags={
+    Name:"${var.env_prefix}-default-sg"
+  }
+  ingress  {
+    from_port = "22"
+    to_port = "22"
+    protocol = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+  egress  {
+    from_port = "0"
+    to_port = "0"
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids=[]
+  }
+
+  
 }
